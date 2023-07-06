2956c9a5fbc9f27ccedb207f76a9cd02760032d2
USRP3 <k.andrzej91@gmail.com>
2023-07-06 20:03:00 +0200

    cokolwiek

--- petle.py
+++ petle.py
@@ -1,4 +1,13 @@
 jezyki = ["C","Python","Java"]
 cyfry = [1,2,3,4,5,6,7,8,9]
-print("jezykiProgramowania")
-for x in range(jezyki)
+
+print("jezyki Programowania:")
+for x in range(len(jezyki)):
+    print(jezyki[x])
+
+for x in range(1,len(cyfry),2):
+    print(f'Cyfry: {cyfry[x]}')
+
+
+print(*cyfry, sep='-')
+print(cyfry)
